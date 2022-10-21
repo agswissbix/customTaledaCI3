f55377d8f74ff1077382e8dcf0a84107b281514a
agswissbix <108857221+agswissbix@users.noreply.github.com>
2022-10-21 16:20:57 +0200

    Update scan_combined_idscanner.php

--- application/views/sys/desktop/custom/taleda/scan_combined_idscanner.php
+++ application/views/sys/desktop/custom/taleda/scan_combined_idscanner.php
@@ -1,8 +1,8 @@
 <div style="text-align: center;width: 100%;margin-top: 100px;">
-    <img style="height: 20%" src="<?=server_url()."custom/taleda/idscanner/caricati/documento1.jpg"?>" style="display: block;margin: auto;">
+    <img style="height: 18%" src="<?=server_url()."custom/taleda/idscanner/caricati/documento1.jpg"?>" style="display: block;margin: auto;">
 </div>
 <br/><br/>
-<div style="text-align: center;width: 100%">
-    <img style="height: 30%" src="<?=server_url()."custom/taleda/idscanner/caricati/oggetto.png"?>" style="display: block;margin: auto;">
+<div style="text-align: center;width: 90%;margin: auto;">
+    <img style="width: 100%" src="<?=server_url()."custom/taleda/idscanner/caricati/oggetto.png"?>" style="display: block;margin: auto;">
 </div>
 
